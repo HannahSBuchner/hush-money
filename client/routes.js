36c787e309509c8dded5edc26d4ca9bba7f4370d
Hannah Sanderson <hsbuchner@gmail.com>
2020-04-26 17:49:51 -0400

    trying to fix routes for deploy

--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {
+  Navbar,
   Login,
   Signup,
   UserHome,
@@ -26,6 +27,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/" component={Navbar} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
